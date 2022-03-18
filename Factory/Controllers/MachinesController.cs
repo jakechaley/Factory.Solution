3de832a3c35bb1cad42c394f634c83107db6e6ec
jakechaley <jakechaley@gmail.com>
2022-03-18 10:59:24 -0700

    add code to EngineersController and HomeController. Start work on MachinesController

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      return View();
+    }
+  }
+}
